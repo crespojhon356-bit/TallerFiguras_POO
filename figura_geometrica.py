bdd3d452a400607c8f61b211f0c2173d73977648
crespojhon356-bit <crespojhon356@gmail.com>
2025-11-20 22:09:30 -0500

    Implement FiguraGeometrica class with properties

---
+++ figura_geometrica.py
@@ -0,0 +1,33 @@
+class FiguraGeometrica:
+    def __init__(self, ancho, alto):
+        self.ancho = ancho
+        self.alto = alto
+
+    @property
+    def ancho(self):
+        return self._ancho
+
+    @ancho.setter
+    def ancho(self, valor):
+        if valor <= 0:
+            raise ValueError("El ancho debe ser mayor que 0.")
+        self._ancho = valor
+
+    @property
+    def alto(self):
+        return self._alto
+
+    @alto.setter
+    def alto(self, valor):
+        if valor <= 0:
+            raise ValueError("El alto debe ser mayor que 0.")
+        self._alto = valor
+
+    def area(self):
+        return self.ancho * self.alto
+
+    def perimetro(self):
+        pass
+
+    def __str__(self):
+        return f"FiguraGeometrica [Ancho: {self.ancho}, Alto: {self.alto}]"
