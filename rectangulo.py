c008e013600858a4b2508ace63f1be7025ce2c8f
crespojhon356-bit <crespojhon356@gmail.com>
2025-11-20 22:11:28 -0500

    Add Rectangulo class for geometric calculations

---
+++ rectangulo.py
@@ -0,0 +1,14 @@
+from figura_geometrica import FiguraGeometrica
+
+class Rectangulo(FiguraGeometrica):
+    def __init__(self, ancho, alto):
+        super().__init__(ancho, alto)
+
+    def area(self):
+        return self.ancho * self.alto
+
+    def perimetro(self):
+        return 2 * (self.ancho + self.alto)
+
+    def __str__(self):
+        return f"Rectangulo [Ancho: {self.ancho}, Alto: {self.alto}]"
